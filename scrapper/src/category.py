dc8bb0056d04a27c450afffe5b858265ca51316d
Ewik <s188636@student.pg.edu.pl>
2023-12-01 01:41:11 +0100

    products csv

--- scrapper/src/category.py
+++ scrapper/src/category.py
@@ -44,8 +44,8 @@ def getCategory(webObject, prevName, savedCategories):
 
 def saveCategoriesToCsv(categories):
     headers = ['ID', 'Active (0/1)', 'Name', 'Parent category']
-
-    with open('categories.csv', 'w', encoding='UTF-8') as f:
+    
+    with open('../scrapped/categories.csv', 'w', encoding='UTF-8') as f:
         writer = csv.writer(f, delimiter =';', lineterminator="\n")
         writer.writerow(headers)
         for categoryName in categories:
--- scrapper/src/product.py
+++ scrapper/src/product.py
@@ -11,7 +11,7 @@ class Product:
     price: float
     manufacturer: str
     specs: list
-    imgURL: str
+    imgURLs: []
 
 
     id: int
@@ -20,6 +20,8 @@ class Product:
 
     def __init__(self, productURL):
         self.__productURL = productURL
+        self.imgURLs = []
+        self.manufacturer = ''
         self._initialize()
         
 
@@ -34,7 +36,7 @@ class Product:
         self.id = soup.find('button', class_='availability-notifier-btn btn btn-red')['data-product-id']
         self.category = mainContainer.find('div', id='box_productfull')['data-category']
         self.description =  mainContainer.find('div', itemprop="description").find('p').text.strip()
-        self.price = mainContainer.find('div', class_ ='price').find('em', class_='main-price').text.replace('\xa0zł', '').replace(',', '.')
+        self.price = mainContainer.find('div', class_ ='price').find('em', class_='main-price').text.replace(' zł', '').replace(',', '.').replace(' ', '')
         self._fetchProductAttr(mainContainer)
         self._fetchGallery(mainContainer)
 
@@ -52,6 +54,8 @@ class Product:
             columns = row.find_all('td')
             rowName = columns[0].text.strip()
             rowValue = columns[1].text.strip()
+            if rowName == 'Producent':
+                self.manufacturer = rowValue
             attrStruct = {'name': rowName,
                             'value': rowValue}
             productSpecs.append(attrStruct)
@@ -71,15 +75,17 @@ class Product:
             self._saveImg('0', BASE_URL+(mainContainer.find('img', class_='js__open-gallery')['src']))
 
     def _saveImg(self, imgName:str, imgURL:str):
+        if not ('thumbnail' in imgName):
+            self.imgURLs.append(imgURL)
         #ignore already downloaded imgs
-        if os.path.exists(f'scrapper\scrapped\img\{self.id}\{imgName}.jpg'): return
+        if os.path.exists(f'../scrapped/img/{self.id}/{imgName}.jpg'): return
         req = self._getPageResponse(imgURL).content
         try:
             req = str(req, 'utf-8')
         except UnicodeDecodeError:
-            if not os.path.exists(f'scrapper\scrapped\img\{self.id}'):
-                os.mkdir(f'scrapper\scrapped\img\{self.id}')
-            with open(f'scrapper\scrapped\img\{self.id}\{imgName}.jpg', 'wb+') as f:
+            if not os.path.exists(f'../scrapped/img/{self.id}'):
+                os.mkdir(f'../scrapped/img/{self.id}')
+            with open(f'../scrapped/img/{self.id}/{imgName}.jpg', 'wb+') as f:
                 f.write(req)
 
     @staticmethod
@@ -92,3 +98,75 @@ class Product:
             except:
                 retries+=1
                 sleep(5)
+    
+    def writeToCsv(self, writer):
+        imgUrls = ""
+        for i in range(len(self.imgURLs)):
+            if i == 0:
+                imgUrls = self.imgURLs[i]
+            imgUrls+=', ' + self.imgURLs[i]
+        
+        features = ""
+        for feature in self.specs:
+            features+=f"{feature['name']}: {feature['value']},"
+        if len(features) > 0:
+            features = features[:-1]
+        
+        if len(self.name) > 128:
+            self.name = self.name[:128]
+
+        writer.writerow([self.id, #ID
+                          1, #Active
+                          self.name, #Name
+                          self.category, #Categories
+                          self.price, #Price tax excluded
+                          '', #Tax rule ID
+                          #self.price, #Price tax included
+                          self.price, #Cost price
+                          0, #On sale (0/1)
+                          0, #Discount amount
+                          0, #Discount percent
+                          '', #Discount from (yyyy-mm-dd)
+                          '', #Discount to (yyyy-mm-dd)
+                          '', #Reference #
+                          '', #Supplier reference #
+                          '', #Supplier
+                          self.manufacturer, #Brand
+                          '', #EAN13
+                          '', #UPC
+                          '', #MPN
+                          '', #Ecotax
+                          '', #Width
+                          '', #Height
+                          '', #Depth
+                          '', #Weight
+                          '', #Delivery time of in-stock products:
+                          '', #Delivery time of out-of-stock products with allowed orders:
+                          '', #Quantity
+                          '', #Minimal quantity
+                          '', #Low stock level
+                          '', #Send me an email when the quantity is under this level
+                          '', #Visibility
+                          '', #Additional shipping cost
+                          '', #Unit for base price
+                          self.price, #Base price
+                          self.price, #Summary
+                          self.description, #Description
+                          '', #Tags
+                          '', #Meta title
+                          '', #Meta keywords
+                          '', #Meta description
+                          '', #Rewritten URL
+                          '', #Label when in stock
+                          '', #Label when backorder allowed
+                          1, #Available for order (0 = No, 1 = Yes)
+                          '', #Product availability date
+                          '', #Product creation date
+                          1, #Show price (0 = No, 1 = Yes)/
+                          imgUrls, #Image URLs (x,y,z...)
+                          '', #Image alt texts (x,y,z...)
+                          1, #Delete existing images (0 = No, 1 = Yes)
+                          features, #Feature (Name:Value:Position:Customized)
+                          0, #Available online only (0 = No, 1 = Yes)
+                          'new', #Condition
+                          ])
