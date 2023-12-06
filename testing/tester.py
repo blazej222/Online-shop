7acd841b5c284cd905d5f921ebac9e27d85eacc5
LotusPingwin <szybkipingwin@gmail.com>
2023-12-06 23:14:24 +0100

    Added initial test files

--- testing/tester.py
+++ testing/tester.py
@@ -1,2 +1,13 @@
-message = input('Enter a message:')
-print(f'Your name is: {message}')
+script_list = ''' 
+a) Adding 10 products with varying quantities
+b) Search up a product and add a random one from the lot
+c) Delete 3 products from the cart
+d) Account registration
+e, f, g, h) Ordering the cart content
+i) Checking the status of an order
+j) Downloading an invoice
+'''
+
+user_response = input('Select a script to run OR type "all" for the whole sequence: ' + script_list)
+print(f'Selected script: {user_response}')
+
