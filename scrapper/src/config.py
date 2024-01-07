b978a8f4a083ca059ce3e0a578d8578755883fc3
LotusPingwin <szybkipingwin@gmail.com>
2024-01-07 09:18:29 +0100

    Config adjustment: new port and a file location

--- scrapper/src/config.py
+++ scrapper/src/config.py
@@ -1,3 +1,4 @@
 directory = '../scrapped/'
-api_url = 'http://prestashop:8080/api'
+output_directory = '../scrapped_reduced/'
+api_url = 'http://prestashop:18863/api'
 api_key = 'IF2AXLH75F1DL3ZLB2S9AJGZQC83SRDK'
