f761649c6a0658efd9cac49202a530c296e3cffe
Blazej222 <firma.bio@gmail.com>
2024-01-07 16:29:53 +0100

    Working build

--- website/apache-config/starter.sh
+++ website/apache-config/starter.sh
@@ -2,8 +2,8 @@
 a2enmod ssl
 # echo "Restarting apache2"
 # service apache2 restart 
-echo "Modding user"
-usermod -u 1000 www-data  
+# echo "Modding user"
+# usermod -u 1000 www-data  
 echo "Starting apache foreground"
 apache2-foreground
 # wait $!
